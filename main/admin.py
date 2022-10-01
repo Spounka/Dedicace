f789cf822bfdd53887fba7ee8174b4e7f3bd3222
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-10-01 08:22:45 +0100

    phone_number migration

--- main/admin.py
+++ main/admin.py
@@ -4,6 +4,13 @@ from .models import User, Celebrity, Fan, Disponibility, Request, Payment
 
 
 class CustomUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (None,
+         {
+             "classes": ("wide",),
+             "fields":  ("phone_number", "username", "password1", "password2"),
+         }),
+    )
     list_display = ("phone_number", "username", "email", "first_name", "last_name", "is_staff")
     ordering = ("phone_number", "username")
 
