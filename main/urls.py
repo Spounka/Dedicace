0793f4dfbfee1a2c78699b08f13895f6c4f6ae87
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-10-01 10:01:15 +0100

    editted names in urls

--- main/urls.py
+++ main/urls.py
@@ -5,9 +5,9 @@ from . import views
 router = DefaultRouter()
 router.register(r"users", views.UserViewSet, basename="user")
 router.register(r"celebrities", views.CelebrityViewSet, basename="celebs")
-router.register(r"fans", views.FanViewSet, basename="celebs")
-router.register(r"requests", views.RequestViewSet, basename="celebs")
-router.register(r"payments", views.PaymentViewSet, basename="celebs")
+router.register(r"fans", views.FanViewSet, basename="fans")
+router.register(r"requests", views.RequestViewSet, basename="requests")
+router.register(r"payments", views.PaymentViewSet, basename="payments")
 
 urlpatterns = [
     path('', include(router.urls))
