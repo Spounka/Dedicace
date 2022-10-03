b7257de9d20877c4e0f5d4cf122ba484c59cae77
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-10-03 02:17:41 +0100

    Fan api view endpoints

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, generics, mixins, response, status
+from rest_framework.views import APIView
+
 from .models import User, Celebrity, Fan, Request, Payment
 from .serializers import UserSerializer, CelebritySerializer, FanSerializer, RequestSerializer, PaymentSerializer
 
@@ -28,3 +29,33 @@ class RequestViewSet(viewsets.ReadOnlyModelViewSet):
 class PaymentViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Payment.objects.all()
     serializer_class = PaymentSerializer
+
+
+class FanAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = FanSerializer
+    queryset = Fan.objects.all()
+
+
+class CreateFanAPIView(APIView, mixins.CreateModelMixin):
+    def create(self, request, *args, **kwargs):
+        serializer = FanSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data.get['phone_number'] = User.normalize_username(request.data['user']['phone_number'])
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return response.Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def post(self, request):
+        if not request.data.get('user'):
+            return response.Response(status=status.HTTP_400_BAD_REQUEST, data={"reason": "user is empty"})
+        user_serializer = UserSerializer(data=request.data['user'])
+        if not user_serializer.is_valid(raise_exception=True):
+            return response.Response(status=status.HTTP_400_BAD_REQUEST, data={"reason": "user issue"})
+        user = user_serializer.save()
+
+        serializer = FanSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.validated_data['user'] = user
+            serializer.save()
+            return response.Response(data={"status": "success"})
+        return response.Response(data={"status": "failed"})
