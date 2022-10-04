21265f5051197c671342058ada9dc7521878e89e
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-10-04 05:27:34 +0100

    Removed Router

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,9 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 urlpatterns = [
-    path(r"user/update/<int:pk>/", views.FanAPIView.as_view(), name="hello"),
-    path(r"users/create/", views.CreateFanAPIView.as_view(), name="oss"),
+    path(r"celebrities/", views.CelebrityAPIView.as_view(), name="celebs"),
+    path(r"user/update/<int:pk>/", views.FanAPIView.as_view(), name="fans"),
+    path(r"users/create/", views.CreateFanAPIView.as_view(), name="something"),
     path(r"current/", views.GetCurrentUser.as_view(), name="logout"),
     path(r"auth/", include("knox.urls")),
 ]
