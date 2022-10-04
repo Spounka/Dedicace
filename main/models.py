57c620d488137760c2e74d56931abf3cf9646c30
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-10-05 00:20:09 +0100

    moved dummy methods to dummy scripts

--- main/models.py
+++ main/models.py
@@ -1,5 +1,3 @@
-import random
-
 from django.apps import apps
 from django.conf import settings
 from django.contrib import auth
@@ -222,36 +220,3 @@ class Report(models.Model):
                                  related_name="report_recipient")
     report_date = models.PositiveBigIntegerField(default=0)
     report_reason = models.TextField(default="Report Reason")
-
-
-def create_dummy_user(i: int, offset: int = 0):
-    phone_number = f"+2130000000{i + offset:02}"
-    password = "rootuser"
-    email = f"admin{i + offset + 1}@admin.com"
-    username = f"username{i + offset + 1}"
-    user = User.objects.create_user(phone_number=phone_number, email=email, password=password, username=username)
-    user.save()
-    return user
-
-
-def create_dummy_celebs(number=10, offset: int = 0):
-    for i in range(number):
-        celeb = Celebrity.objects.create(user=create_dummy_user(i, offset))
-        celeb.save()
-
-
-def create_dummy_fans(number=10, offset: int = 0):
-    for i in range(number):
-        wilaya = random.randint(1, 58)
-        fan = Fan.objects.create(user=create_dummy_user(i, offset), wilaya=f"{wilaya}")
-        fan.save()
-
-
-def dummy(number=10):
-    create_dummy_fans(number // 2)
-    create_dummy_celebs(number // 2, offset=number // 2)
-
-
-def delete_dummy():
-    User.objects.exclude(is_superuser=True).all().delete()
-    return True
