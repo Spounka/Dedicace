a8623624e979dc1298bf6d9d7af9526e41987be5
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-12-05 01:47:11 +0100

    Edited model and add migrations

--- chat/models.py
+++ chat/models.py
@@ -3,26 +3,29 @@ from django.conf import settings
 
 
 # Create your models here.
-class ChatMessage(models.Model):
+class ChatMessageInfo(models.Model):
     date_sent = models.CharField(max_length=60, default="")
-    sender: settings.AUTH_USER_MODEL = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
-    recepient = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+    sender: settings.AUTH_USER_MODEL = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="sent_messages",
+                                                         on_delete=models.SET_NULL, null=True)
+    recepient: settings.AUTH_USER_MODEL = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="received_messages",
+                                                            on_delete=models.SET_NULL, null=True)
 
-    class Meta:
-        abstract = True
 
-
-class TextMessage(ChatMessage):
+class TextMessageInfo(models.Model):
+    message_info = models.OneToOneField(ChatMessageInfo, on_delete=models.CASCADE)
     text_message = models.CharField(max_length=300, default="")
 
 
-def get_media_chat_upload_path(instance: 'ImageMessage', filename):
-    return f"{instance.sender.username}/{instance.recepient.username}/{instance.date_sent}/{filename}"
+def get_media_chat_upload_path(instance: 'ImageMessageInfo', filename):
+    return f"{instance.message_info.sender.username}/{instance.message_info.username}/" \
+           f"{instance.message_info.date_sent}/{filename}"
 
 
-class ImageMessage(ChatMessage):
+class ImageMessageInfo(models.Model):
+    message_info = models.OneToOneField(ChatMessageInfo, on_delete=models.CASCADE)
     image = models.ImageField(upload_to=get_media_chat_upload_path)
 
 
-class VoiceMessage(ChatMessage):
+class VoiceMessageInfo(ChatMessageInfo):
+    message_info = models.OneToOneField(ChatMessageInfo, related_name="voidemessage", on_delete=models.CASCADE)
     audio = models.FileField(upload_to=get_media_chat_upload_path)
