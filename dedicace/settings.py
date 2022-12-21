8e40c376ab448d1755482252851de194bf124c93
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-12-21 22:11:43 +0100

    Added dashboard statics dir

--- dedicace/settings.py
+++ dedicace/settings.py
@@ -139,6 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'dashboard/static'
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
