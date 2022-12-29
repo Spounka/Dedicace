3c6211b02b2a5a9e1595bf659d70f71e9be2e80e
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2022-12-29 18:26:12 +0100

    added fly.io deployment stuff

--- dedicace/settings.py
+++ dedicace/settings.py
@@ -12,19 +12,30 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+import whitenoise.storage
+
+# from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# load_dotenv(BASE_DIR / '.env')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-&_*hcuw7w45=i4-5=%((h2efryk8_^(eqzb*q-2x2*_5%_i*tt'
+# SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", 'dedicace.fly.dev']
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://dedicace.fly.dev'
+]
 
 # Application definition
 
@@ -43,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -91,18 +103,8 @@ WSGI_APPLICATION = 'dedicace.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE':   'django.db.backends.postgresql',
-        # 'NAME':     os.environ.get("POSTGRES_DB"),
-        # 'USER':     os.environ.get("POSTGRES_USER"),
-        # 'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
-        # 'HOST':     "db",
-        'NAME':     "Dedicace",
-        'USER':     "spounka",
-        'PASSWORD': "pass123",
-        'HOST':     "localhost",
-        'PORT':     5432,
-    }
+    # 'default': dj_database_url.parse("postgres://postgres:cfPqoXAeNS8zwAa@dedicace-db.internal:5433/dedicace")
+    'default': dj_database_url.parse(os.environ['DATABASE_URL'])
 }
 
 # Password validation
@@ -139,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'dashboard/static'
 ]
@@ -153,3 +155,4 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 FILE_UPLOAD_PERMISSIONS = 0o644
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
