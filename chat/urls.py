42ae3c07b9a16f0e3fae2e15a7abf448ebb4238b
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-01-20 16:30:01 +0100

    Added Chat path to urls
    Signed-off-by: Nazih Boudaakkar <boudaakkarnazih@gmail.com>

--- chat/urls.py
+++ chat/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('chat/<int:pk>/', views.DiscussionAPIView.as_view(), name="chat-room"),
     path('text/<int:pk>/', views.TextMessageAPIView.as_view(), name='text-chat'),
     path('voice/<int:pk>/', views.VoiceMessageAPIView.as_view(), name='voice-chat'),
     path('image/<int:pk>/', views.ImageMessageAPIView.as_view(), name='image-chat'),
