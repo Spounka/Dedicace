55bc660e2b111d2eda406d855fda47c6818fe1ba
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-01-20 16:27:10 +0100

    Code formatting
    Signed-off-by: Nazih Boudaakkar <boudaakkarnazih@gmail.com>

--- chat/models.py
+++ chat/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.conf import settings
+from django.db import models
 
 
 # Create your models here.
@@ -12,20 +12,23 @@ class ChatMessageInfo(models.Model):
 
 
 class TextMessage(models.Model):
-    message_info = models.OneToOneField(ChatMessageInfo, on_delete=models.CASCADE)
+    message_info = models.OneToOneField(
+        ChatMessageInfo, on_delete=models.CASCADE)
     text_message = models.CharField(max_length=300, default="")
 
 
 def get_media_chat_upload_path(instance, filename):
     return f"{instance.message_info.sender.username}/{instance.message_info.recepient.username}/" \
-           f"{instance.message_info.date_sent}/{filename}"
+        + f"{instance.message_info.date_sent}/{filename}"
 
 
 class ImageMessage(models.Model):
-    message_info = models.OneToOneField(ChatMessageInfo, on_delete=models.CASCADE)
+    message_info = models.OneToOneField(
+        ChatMessageInfo, on_delete=models.CASCADE)
     image = models.ImageField(upload_to=get_media_chat_upload_path)
 
 
 class VoiceMessage(models.Model):
-    message_info = models.OneToOneField(ChatMessageInfo, on_delete=models.CASCADE)
+    message_info = models.OneToOneField(
+        ChatMessageInfo, on_delete=models.CASCADE)
     audio = models.FileField(upload_to=get_media_chat_upload_path)
