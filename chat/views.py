9611a4d22c81811fa0cf5f4b626ed15bcf1ac8fc
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-01-21 20:19:43 +0100

    Added single discussion view
    Signed-off-by: Nazih Boudaakkar <boudaakkarnazih@gmail.com>

--- chat/views.py
+++ chat/views.py
@@ -16,7 +16,22 @@ logger.setLevel(logging.DEBUG)
 class DiscussionAPIView(generics.ListAPIView):
     permission_classes = [IsAuthenticated]
     authentication_classes = (TokenAuthentication,)
-    serializer_class = serializers.MessageInfoSerializer
+    serializer_class = serializers.DiscussionSerializer
+    queryset = models.Discussion.objects.all
+
+    def get(self, request, *args, **kwargs):
+        if (pk := kwargs.get('pk')) is not None:
+            try:
+                discussion = models.Discussion.objects.get(pk=pk)
+                messages = discussion.chatmessageinfo_set.all()
+                message: models.ChatMessageInfo = messages.first()
+                result = serializers.MessageInfoSerializer(messages, many=True)
+                logger.info(f'found discussion with id {discussion.pk}')
+                return response.Response(status=status.HTTP_200_OK, data=result.data)
+            except models.Discussion.DoesNotExist:
+                logger.error(f'discussion with pk {pk} does not exist')
+                return response.Response(status=status.HTTP_404_NOT_FOUND)
+        return super().get(request, *args, **kwargs)
 
     def list(self, request, *args, **kwargs):
         user: settings.AUTH_USER_MODEL = request.user
