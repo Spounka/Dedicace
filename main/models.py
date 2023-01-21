cb3982b5d4cebb9caf23f3e0565497237b1f7d65
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-01-21 22:41:00 +0100

    Added Title to OfferRequest
    Signed-off-by: Nazih Boudaakkar <boudaakkarnazih@gmail.com>

--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+
 from .manager import UserManager
 
 WILAYA_CHOICES = [
@@ -199,6 +200,7 @@ class OfferRequest(models.Model):
     recepient: User = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
                                         related_name="request_recepient")
     description = models.TextField()
+    title = models.CharField(max_length=100, default="Title")
 
     def __str__(self):
         return f"{self.sender.username}-{self.recepient}"
