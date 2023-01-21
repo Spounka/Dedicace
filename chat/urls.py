03ae8afd4de50b424a9ec379d841cf1d787561c0
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-01-21 22:44:27 +0100

    replaced redundant urls with one message endpoint
    Signed-off-by: Nazih Boudaakkar <boudaakkarnazih@gmail.com>

--- chat/urls.py
+++ chat/urls.py
@@ -5,7 +5,5 @@ from . import views
 urlpatterns = [
     path('discussions/', views.DiscussionAPIView.as_view(), name="discussions"),
     path('discussions/<int:pk>/', views.DiscussionAPIView.as_view(), name="discussion"),
-    path('text/<int:pk>/', views.TextMessageAPIView.as_view(), name='text-chat'),
-    path('voice/<int:pk>/', views.VoiceMessageAPIView.as_view(), name='voice-chat'),
-    path('image/<int:pk>/', views.ImageMessageAPIView.as_view(), name='image-chat'),
+    path('message/', views.MessageInfoAPIView.as_view(), name="message")
 ]
