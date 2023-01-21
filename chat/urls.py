44eb25dce085013df4f56f8532f319a10cd4f3b6
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-01-21 20:17:05 +0100

    changed url paths for the chat
    Signed-off-by: Nazih Boudaakkar <boudaakkarnazih@gmail.com>

--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('chat/<int:pk>/', views.DiscussionAPIView.as_view(), name="chat-room"),
+    path('discussions/', views.DiscussionAPIView.as_view(), name="discussions"),
+    path('discussions/<int:pk>/', views.DiscussionAPIView.as_view(), name="discussion"),
     path('text/<int:pk>/', views.TextMessageAPIView.as_view(), name='text-chat'),
     path('voice/<int:pk>/', views.VoiceMessageAPIView.as_view(), name='voice-chat'),
     path('image/<int:pk>/', views.ImageMessageAPIView.as_view(), name='image-chat'),
