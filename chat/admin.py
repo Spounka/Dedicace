7ddbfc00c7a9e6346ce79e61e89d5f83821f45a6
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-01-24 23:20:22 +0100

    Admin site chat utilities
    Signed-off-by: Nazih Boudaakkar <boudaakkarnazih@gmail.com>

--- chat/admin.py
+++ chat/admin.py
@@ -2,8 +2,19 @@ from django.contrib import admin
 
 from . import models
 
+
 # Register your models here.
-admin.site.register(models.Discussion)
+class ChatMessageInfoInline(admin.TabularInline):
+    model = models.ChatMessageInfo
+
+
+@admin.register(models.Discussion)
+class DiscussionAdmin(admin.ModelAdmin):
+    inlines = [
+        ChatMessageInfoInline
+    ]
+
+
 admin.site.register(models.ChatMessageInfo)
 admin.site.register(models.TextMessage)
 admin.site.register(models.VoiceMessage)
