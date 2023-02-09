af83ff99ff6bbe580934c792c0b4547f8760a041
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-02-09 02:02:28 +0100

    Removed stupid fly.io stuff messing with dev env

--- dedicace/settings.py
+++ dedicace/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -191,5 +190,3 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-FILE_UPLOAD_PERMISSIONS = 0o644
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
