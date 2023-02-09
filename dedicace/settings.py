48e8d606133aa1fbd610a6fa239054fed2cf9a5e
Nazih Boudaakkar <boudaakkarnazih@gmail.com>
2023-02-09 02:02:15 +0100

    Removed stupid fly.io stuff messing with dev env

--- dedicace/settings.py
+++ dedicace/settings.py
@@ -13,9 +13,6 @@ import datetime
 import os
 from pathlib import Path
 
-import dj_database_url
-import whitenoise.storage
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
